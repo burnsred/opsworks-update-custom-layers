618677d251a1a781b98c94358a373c6e274cbc0a
Michael Bertolacci <m.bertolacci@gmail.com>
2014-11-03 17:51:59 +1100

    Add boto as dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opsworks-update-custom-layers',
-    version='0.0.2',
+    version='0.0.3',
     packages=['opsworks_update_custom_layers',],
     scripts=['bin/opsworks-update-custom-layers'],
     include_package_data=True,
@@ -11,4 +11,5 @@ setup(
     author_email='michael@burnsred.com.au',
     url='',
     long_description=open('README.md').read(),
+    install_requires=['boto==2.34.0'],
 )
